8ef003f2c0981e14e41d660851431584164fae8f
Joshua Martinez <j1995martinez@gmail.com>
2018-10-04 20:07:57 -0700

    going to work on desktop

--- homework2/scriptJava.js
+++ homework2/scriptJava.js
@@ -1,4 +1,7 @@
-function addition()
-{
-    
+function check() {
+    var sex, lastN;
+    lastN = document.getElementById("lN").value;
+    firstN = document.getElementById("fN").value;
+    sex = document.querySelector('input[name = "gender"]:checked').value;
+    alert(lastN + firstN+sex);
 }
