a6e31a3a77ee1f8ee8eecaae1b8e4ff1280a55f2
Joshua Martinez <j1995martinez@gmail.com>
2018-10-09 23:20:38 -0700

    adding QueueInterface.cs

---
+++ homework3/main/main/QueueInterface.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class hw3
+{
+    /// <summary>
+    /// a FIFO queu interface. For a singly linked queue
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+	public interface QueueInterface<T>
+	{
+        ///Add an element to the end of the queue
+        ///
+        /// return the elemtn that was enqueued
+        ///
+        
+        T push(T element);
+
+        /// removes and returns the front element
+        /// 
+        /// throws Thrown if the queue is empty
+
+        
+        T pop();
+
+        bool isEmpty();
+	}
+}
