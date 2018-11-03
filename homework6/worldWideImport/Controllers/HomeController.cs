2a5b5c5ab32ef362017a3320f542aea3030905b3
Joshua Martinez <j1995martinez@gmail.com>
2018-11-02 18:33:38 -0700

    starting new branch for project that is already started

---
+++ homework6/worldWideImport/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using worldWideImport.DAL;
+using worldWideImport.Models;
+
+namespace worldWideImport.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        UserContext db = new UserContext();
+
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string searchString)
+        {
+            ViewBag.result = searchString;
+
+            var test = db.People.Take(10);
+            System.Diagnostics.Debug.WriteLine(searchString);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
