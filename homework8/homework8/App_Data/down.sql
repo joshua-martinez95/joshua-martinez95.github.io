b93a59df7273e72c7496d4f19cc7bdfa7ef0b5a3
Joshua Martinez <j1995martinez@gmail.com>
2018-11-18 18:38:30 -0800

    Done setting up table

---
+++ homework8/homework8/App_Data/down.sql
@@ -0,0 +1,4 @@
+﻿DROP TABLE [dbo].[Bids]; /* Input table names*/
+DROP TABLE [dbo].[Items]; /* Input table names */
+DROP TABLE [dbo].[Sellers]; /* Input table names*/
+DROP TABLE [dbo].[Buyers]; /* Input table names*/
